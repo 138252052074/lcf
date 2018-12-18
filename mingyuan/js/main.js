a1a1e83ccf1be925b4b1ec8cb8cf3c5ac6c23312
laicanfu <1066441234@qq.com>
2018-12-18 11:05:55 +0800

    first commit

---
+++ mingyuan/js/main.js
@@ -0,0 +1,23 @@
+window.onload=function (){
+//	add_nav_active();
+//	var oUl=document.getElementById('add_nav_active');
+//	oUl.onclick=function (){
+//		alert("f");
+//	}
+};
+
+//导航栏添加active
+//function add_nav_active(){
+//	var oUl=document.getElementById('add_nav_active');
+//	var aLi=oUl.getElementsByTagName('li');
+//	
+//	for(var i=0;i<=aLi.length;i++){
+//		aLi[i].onclick=function (){
+//			for(var j=0;j<=aLi.length;j++){
+//				aLi[j].removeAttribute('class','active');
+//			}
+//			this.setAttribute('class','active');
+//		}
+//		
+//	}
+//}
